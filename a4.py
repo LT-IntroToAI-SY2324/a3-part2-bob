d8a3ed257bbc0b6a70643ca0f100800fdfb8e213
Jane Kim <jkim21@cps.edu>
2023-10-12 11:12:54 -0500

    first chunk of asserts

--- a4.py
+++ a4.py
@@ -19,31 +19,88 @@ def schools_by_year(matches: List[str]) -> List[str]:
         if int(matches[0]) == get_year(school):
             results.append(get_name(school))
             return results
+        
 def schools_by_year_range(matches: List[str]) -> List[str]:
     results = []
     for school in school_db:
         if int(matches[0]) <= get_year(school):
             results.append(get_name(school))
             return results
+        
 def schools_before_year(matches: List[str]) -> List[str]:
     results = []
     for school in school_db:
         if get_year(school)< int(matches[0]):
             results.append(get_name(school))
             return results
+        
 def schools_after_year(matches: List[str]) -> List[str]:
     results = []
     for school in school_db:
         if get_year(school)< int(matches[0]):
             results.append(get_name(school))
             return results
+        
 def schools_by_rate_higher(matches: List[str]) -> List[str]:
+    results=[]
+    for school in school_db:
+        if get_rate(school)>int(matches[0]):
+            results.append(get_name(school))
+    return results
+
 def schools_by_rate_lower(matches: List[str]) -> List[str]:
+    results=[]
+    for school in school_db:
+        if get_rate(school)<int(matches[0]):
+            results.append(get_name(school))
+    return results
+
 def schools_by_rate_range(matches: List[str]) -> List[str]:
-def schools_by_size_bigger(matches: List[str]) -> List[str]:
+    results=[]
+    for school in school_db:
+        if int(matches[0])<= get_rate(school)<=int(matches[1]):
+            results.append(get_name(school))
+    return results
+
+def schools_by_size_bigger(matches: List[str]) -> List[str]: 
+    results=[]
+    for school in school_db:
+        if get_size(school)>int(matches[0]):
+            results.append(get_name(school))
+    return results
+
 def schools_by_size_smaller(matches: List[str]) -> List[str]:
+    to_return = []
+    #Iterates through School_db where School is one Schools worth of data
+    for school in school_db:
+        #If the size of the school is less than the first element in matches
+        if (get_size(school) < int(matches[0])):
+            to_return.append(get_name(school))
+    return to_return
+    
 def schools_by_size_range(matches: List[str]) -> List[str]:
+     #Includes boths bounds
+    to_return = []
+    # orders matches from smallest to largest
+    if (matches[0] > matches[1]):
+        place_holder = matches[0]
+        matches[0] = matches[1]
+        matches[1] = place_holder
+    #Iterates through School_db where School is one Schools worth of data
+    for school in school_db:
+        #If the size of the school is greater than or equal to the first element in matches and less than or equal to the second element of matches
+        if (get_size(school) >= matches[0] and get_size(school) <= matches[1]):
+            to_return.append(get_name(school))
+    return to_return
+
 def schools_by_location(matches: List[str]) -> List[str]:
+    to_return = []
+    #Iterates through School_db where School is one Schools worth of data
+    for school in school_db:
+        #If the location of the school is the same as element 1 of matches
+        if (get_location(school) == matches[0]):
+            to_return.append(get_name(school))
+        return to_return
 
 
 pa_list: List[Tuple[List[str], Callable[[List[str]], List[Any]]]] = [
@@ -64,6 +121,18 @@ pa_list: List[Tuple[List[str], Callable[[List[str]], List[Any]]]] = [
 
 ]
 
+if __name__ == "__main__":
+    assert isinstance(schools_by_year(["1746"]), list), "schools_by_year not returning a list"
+    assert isinstance(schools_by_year_range(["1770", "1850"]), list), "schools_by_year_range not returning a list"
+    assert isinstance(schools_before_year(["1800"]), list), "schools_before_year not returning a list"
+    assert isinstance(schools_after_year(["1700"]), list), "schools_after_year not returning a list"
+    assert isinstance(schools_by_rate_higher(["20.5"]), list), "schools_by_rate_higher not returning a list"
+    assert isinstance(schools_by_rate_lower(["73.4"]), list), "schools_by_rate_lower not returning a list"
+    assert isinstance(schools_by_rate_range(["4", "60.7"]), list), "schools_by_rate_range not returning a list"
+    assert isinstance(schools_by_size_bigger(["20,000"]), list), "schools_by_size_bigger not returning a list"
+    assert isinstance(schools_by_size_smaller(["30,000"]), list), "schools_by_size_smaller not returning a list"
+    assert isinstance(schools_by_size_range(["10,000", "17,000"]), list), "schools_by_size_range not returning a list"
+    assert isinstance(schools_by_location([ "Norman, Oklahoma, USA"]), list), "schools_by_location not returning a list"
 #School name
     #location
     #year founded
