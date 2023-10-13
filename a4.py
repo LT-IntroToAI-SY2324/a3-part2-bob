3c5753d02cf5e81005b363dcf83515332cbd33bc
Jane Kim <jkim21@cps.edu>
2023-10-13 11:06:37 -0500

    fixed comma issue

--- a4.py
+++ a4.py
@@ -121,6 +121,45 @@ pa_list: List[Tuple[List[str], Callable[[List[str]], List[Any]]]] = [
 
 ]
 
+def search_pa_list(src: List[str]) -> List[str]:
+    """Takes source, finds matching pattern and calls corresponding action. If it finds
+    a match but has no answers it returns ["No answers"]. If it finds no match it
+    returns ["I don't understand"].
+
+    Args:
+        source - a phrase represented as a list of words (strings)
+
+    Returns:
+        a list of answers. Will be ["I don't understand"] if it finds no matches and
+        ["No answers"] if it finds a match but no answers
+    """
+    for pat, act in pa_list:
+        mat=match(pat,src)
+        if mat is not None: 
+            answer=act(mat)
+            return answer if answer else["No answers"]
+        return["I don't understand"]
+
+
+def query_loop() -> None:
+    """The simple query loop. The try/except structure is to catch Ctrl-C or Ctrl-D
+    characters and exit gracefully.
+    """
+    print("Welcome to the school database!\n")
+    while True:
+        try:
+            print()
+            query = input("Your query? ").replace("?", "").lower().split()
+            answers = search_pa_list(query)
+            for ans in answers:
+                print(ans)
+
+        except (KeyboardInterrupt, EOFError):
+            break
+
+    print("\nSo long!\n")
+
+
 if __name__ == "__main__":
     assert isinstance(schools_by_year(["1746"]), list), "schools_by_year not returning a list"
     assert isinstance(schools_by_year_range(["1770", "1850"]), list), "schools_by_year_range not returning a list"
@@ -133,6 +172,10 @@ if __name__ == "__main__":
     assert isinstance(schools_by_size_smaller(["30,000"]), list), "schools_by_size_smaller not returning a list"
     assert isinstance(schools_by_size_range(["10,000", "17,000"]), list), "schools_by_size_range not returning a list"
     assert isinstance(schools_by_location([ "Norman, Oklahoma, USA"]), list), "schools_by_location not returning a list"
+
+
+    
+
 #School name
     #location
     #year founded
